577ddc27a022b9378841f8db41cfdbb8c14a04fa
Diony Severino <dls2307@g.rit.edu>
2021-09-12 09:48:25 -0400

    Finished streaming media assignment

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -1,11 +1,24 @@
 const fs = require('fs');
 
-const index = fs.readFileSync(`${__dirname}/../client/client.html`);
-
-const getIndex = (request, response) => {
+const loadPage = (request, response, filePath) => {
+  const index = fs.readFileSync(`${__dirname}/${filePath}`);
   response.writeHead(200, { 'Content-Type': 'text/html' });
   response.write(index);
   response.end();
 };
 
+const getIndex = (request, response) => {
+  loadPage(request, response, '../client/client.html');
+};
+
+const getPage2 = (request, response) => {
+  loadPage(request, response, '../client/client2.html');
+};
+
+const getPage3 = (request, response) => {
+  loadPage(request, response, '../client/client3.html');
+};
+
 module.exports.getIndex = getIndex;
+module.exports.getPage2 = getPage2;
+module.exports.getPage3 = getPage3;
